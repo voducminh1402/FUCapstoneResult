59a354ca6a5c251cd9fea2b02900c574c06a2cec
huylocson <huylocson123@gmail.com>
2022-01-11 15:50:36 +0700

    mod-requset

--- web/assets/js/mod-request.js
+++ web/assets/js/mod-request.js
@@ -12,3 +12,11 @@ $(function ($) {
     // bulma end
 
 });
+
+//  modal
+var myModal = document.getElementById('myModal')
+var myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', function () {
+    myInput.focus()
+})
